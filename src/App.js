f34a85ecfef7aa55d209f07cfd118dc80c0c95ca
Frank Lee <somerlee.9@gmail.com>
2022-12-08 01:42:13 +0800

    :bug: fixed routing bug

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate,
