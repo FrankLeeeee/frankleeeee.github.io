5a639ce79f441b8fde75e07b43f2ccd601583fd2
Frank Lee <somerlee.9@gmail.com>
2023-08-10 02:52:37 +0800

    :bug: fixed the format (#3)

--- pages/publications.js
+++ pages/publications.js
@@ -9,19 +9,22 @@ export default function Publications() {
 
   return (
     <Layout>
-      <div className="flex justify-center">
+      <div className="container mx-auto pt-4 md:pt-12 md:px-12 lg:pt-24 lg:px-24">
+        <h1 className="text-white text-4xl pb-4">
+          <strong className="purple">Selected Publications </strong>
+        </h1>
         <PublicationList />
-      </div>
-      <div className="flex w-full justify-center text-center text-white mt-8">
-        <a
-          href={googleScholarURL}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center rounded bg-transparent border-white text-white px-2 py-2 font-semibold ring-1 ring-inset ring-white"
-        >
-          <ArrowRightCircleIcon className="h-8 mr-2" />
-          <span>View My Goold Scholars</span>
-        </a>
+        <div className="flex w-full justify-center text-center text-white mt-8">
+          <a
+            href={googleScholarURL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center rounded bg-transparent border-white text-white px-2 py-2 font-semibold ring-1 ring-inset ring-white"
+          >
+            <ArrowRightCircleIcon className="h-8 mr-2" />
+            <span>View My Goold Scholars</span>
+          </a>
+        </div>
       </div>
     </Layout>
   );
