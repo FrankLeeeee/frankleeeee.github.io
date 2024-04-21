43a651f372139336d796fbd728d72b43f434aae2
Frank Lee <somerlee.9@gmail.com>
2024-04-21 20:35:09 +0800

    :bug: fixed the link color

--- components/NewsList/index.js
+++ components/NewsList/index.js
@@ -1,10 +1,10 @@
 const NewsList = ({ data, keep_recent = 10 }) => {
   data = data.slice(0, keep_recent);
   return (
-    <ul className="list-disc list-inside flex-col">
+    <ul className="list-disc list-inside">
       {data.map((item, idx) => (
         <li className="text-slate-100" key={idx}>
-          {item.date}: {item.emoji} {item.text}
+          {item.date}: {item.emoji} <div>{item.text()}</div>
         </li>
       ))}
     </ul>
