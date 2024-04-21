fe1285aef5bc746b8dfdd2d65219b75a76ac77c7
Frank Lee <somerlee.9@gmail.com>
2024-04-21 19:36:15 +0800

    :rotating_light: fixed compilation warnings

--- components/NewsList/index.js
+++ components/NewsList/index.js
@@ -1,9 +1,9 @@
 const NewsList = ({ data, keep_recent = 10 }) => {
   data = data.slice(0, keep_recent);
   return (
-    <ul class="list-disc list-inside flex-col">
-      {data.map((item, _) => (
-        <li className="text-slate-100">
+    <ul className="list-disc list-inside flex-col">
+      {data.map((item, idx) => (
+        <li className="text-slate-100" key={idx}>
           {item.date}: {item.emoji} {item.text}
         </li>
       ))}
