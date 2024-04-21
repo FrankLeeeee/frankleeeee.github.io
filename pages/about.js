7f128038b451949f8980ee034a537a3d9a52772b
Frank Lee <somerlee.9@gmail.com>
2024-04-21 23:53:57 +0800

    :lipstick: added section layout to about

--- pages/about.js
+++ pages/about.js
@@ -14,16 +14,18 @@ function About() {
   return (
     <Layout>
       <div>
-        <div className="flex flex-col">
-          <div className="flex flex-col md:justify-between md:flex-row flex-wrap">
-            <Aboutcard />
-            <img
-              src="/assets/about.png"
-              alt="about"
-              className="my-8 md:mt-0 object-scale-down max-h-48 md:max-h-64 lg:max-h-96"
-            />
+        <Section title="About Me">
+          <div className="flex flex-col">
+            <div className="flex flex-col md:justify-between md:flex-row flex-wrap">
+              <Aboutcard />
+              <img
+                src="/assets/about.png"
+                alt="about"
+                className="my-8 md:mt-0 object-scale-down max-h-48 md:max-h-64 lg:max-h-96"
+              />
+            </div>
           </div>
-        </div>
+        </Section>
 
         <Section title="News">
           <NewsList data={news} />
