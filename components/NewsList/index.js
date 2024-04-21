18e015aef3e1e97836e4094200819908f8be1f93
Frank Lee <somerlee.9@gmail.com>
2024-04-21 22:33:51 +0800

    :bug: fixed news color (#20)

--- components/NewsList/index.js
+++ components/NewsList/index.js
@@ -1,9 +1,12 @@
-const NewsList = ({ data, keep_recent = 10 }) => {
-  data = data.slice(0, keep_recent);
+import news from "../../data/news";
+
+const NewsList = ({ keep_recent = 10 }) => {
+  const data = news.slice(0, keep_recent);
+
   return (
     <ul className="list-disc list-inside">
       {data.map((item, idx) => (
-        <li className="text-slate-100 flex" key={idx}>
+        <li className="text-slate-100" key={idx}>
           [{item.date}]&nbsp;&nbsp;{item.emoji}&nbsp;{item.text()}
         </li>
       ))}
