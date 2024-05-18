e41f5ce3fe59e0b7f4ca3827ec9311463c81b1c9
Frank Lee <somerlee.9@gmail.com>
2024-05-18 17:03:04 +0800

    :lipstick: udpated google scholar border color

--- components/GoogleScholarButton/index.js
+++ components/GoogleScholarButton/index.js
@@ -6,7 +6,7 @@ const GoogleScholarButton = ({ url }) => (
       href={url}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex items-center rounded bg-transparent border-white text-slate-400 px-2 py-2 font-semibold ring-1 ring-inset ring-white"
+      className="flex items-center rounded bg-transparent border-slate-400 text-slate-400 px-2 py-2 font-semibold ring-1 ring-inset ring-slate-400"
     >
       <ArrowRightCircleIcon className="h-8 mr-2" />
       <span>View My Google Scholars</span>
