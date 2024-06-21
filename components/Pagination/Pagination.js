b08fe4f27cfc90b6c6f4a9d57eb6e885498b4b5d
Frank Lee <somerlee.9@gmail.com>
2024-06-21 16:40:27 +0800

    :sparkles: added pagination for blogs

---
+++ components/Pagination/Pagination.js
@@ -0,0 +1,92 @@
+import {
+  ArrowLongLeftIcon,
+  ArrowLongRightIcon,
+} from "@heroicons/react/20/solid";
+
+export default function Pagination({
+  currentPage,
+  NumPages,
+  siblingCount,
+  goToPageByIndex,
+}) {
+  // compute the page numbers to display,
+  // we show the current page and its siblings and the first page and last page
+  // the page number should start from 1
+  const pageNumbers = [];
+  for (
+    let i = Math.max(1, currentPage - siblingCount);
+    i <= Math.min(NumPages, currentPage + siblingCount);
+    i++
+  ) {
+    pageNumbers.push(i);
+  }
+
+  const goToPrevPage = (e) => {
+    e.preventDefault();
+    const prevPageIndex = Math.max(1, currentPage - 1);
+    goToPageByIndex(prevPageIndex);
+  };
+
+  const goToNextPage = (e) => {
+    e.preventDefault();
+    const nextPageIndex = Math.min(NumPages, currentPage + 1);
+    goToPageByIndex(nextPageIndex);
+  };
+
+  return (
+    <nav className="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
+      {/* previous button */}
+      <div className="-mt-px flex w-0 flex-1">
+        <button
+          onClick={goToPrevPage}
+          className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+        >
+          <ArrowLongLeftIcon
+            className="mr-3 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
+          Previous
+        </button>
+      </div>
+
+      {/* Show page numbers here, we show:
+        1. the current page index and its sibligns (index starts from 1)
+        2. the first page index and last page index
+      */}
+      <div className="hidden md:-mt-px md:flex">
+        {
+          // show current page and its siblings
+          pageNumbers.map((pageIdx) => {
+            return (
+              <a
+                key={pageIdx}
+                href={`?page=${pageIdx}`}
+                className={`${
+                  pageIdx === currentPage
+                    ? "text-blue-400"
+                    : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                } inline-flex items-center px-4 pt-4 text-sm font-medium`}
+              >
+                {pageIdx}
+              </a>
+            );
+          })
+        }
+      </div>
+
+      {/* next button */}
+      <div className="-mt-px flex w-0 flex-1 justify-end">
+        <button
+          onClick={goToNextPage}
+          className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+        >
+          Next
+          <ArrowLongRightIcon
+            className="ml-3 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
+        </button>
+      </div>
+    </nav>
+  );
+}
--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { useSearchParams, useRouter } from "next/navigation";
 import BlogItem from "../../components/Blog/BlogItem";
 import api from "../../lib/lib";
 import Layout from "../../components/Layout";
 import BlogList from "../../components/Blog/BlogList";
 import Section from "../../components/Section";
 import Fade from "../../components/Animations/Fade";
+import Pagination from "../../components/Pagination/Pagination";
 
 export const getStaticProps = async () => {
   const blogs = api.getAllBlogs([
@@ -18,24 +20,48 @@ export const getStaticProps = async () => {
     "ogImage",
     "content",
   ]);
+
+  const blogsPerPage = 8;
+  const NumPages = Math.ceil(blogs.length / blogsPerPage);
+
   return {
-    props: { blogs },
+    props: { blogs, blogsPerPage, NumPages },
   };
 };
 
 // thoughts are personal blogs which are non-technical
-const BlogPage = ({ blogs }) => {
+const BlogPage = ({ blogs, blogsPerPage, NumPages }) => {
+  // get current page from query params
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const currentPage = parseInt(searchParams.get("page")) || 1;
+
+  const blogsToShow = blogs.slice(
+    (currentPage - 1) * blogsPerPage,
+    currentPage * blogsPerPage
+  );
+
+  const goToPageByIndex = (index) => {
+    router.push("/blogs?page=" + index);
+  };
+
   return (
     <Layout>
       <Section title="My Blogs">
         <BlogList>
-          {blogs.map((blog) => (
+          {blogsToShow.map((blog) => (
             <Fade key={blog.slug}>
               <BlogItem blog={blog} />
             </Fade>
           ))}
         </BlogList>
       </Section>
+      <Pagination
+        currentPage={currentPage}
+        NumPages={NumPages}
+        goToPageByIndex={goToPageByIndex}
+        siblingCount={5}
+      />
     </Layout>
   );
 };
