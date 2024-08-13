11a733da536978a843ee780af568b7370a49d5b9
Frank Lee <somerlee.9@gmail.com>
2024-08-13 17:11:59 +0800

    :memo: updated projects

--- data/projects.js
+++ data/projects.js
@@ -18,7 +18,7 @@ const projects = [
     link: "https://github.com/hpcaitech/ColossalAI",
     name: "ColossalAI",
     badge: "hpcaitech/ColossalAI",
-    description: "Top 1 contributor, little involvement now",
+    description: "Top 1 contributor as of Aug 2024",
   },
 ];
 
