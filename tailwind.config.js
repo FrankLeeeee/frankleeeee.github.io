ab3af0a651604dea034226163c9976fd23702ad9
Frank Lee <somerlee.9@gmail.com>
2024-10-19 15:42:47 +0800

    :memo: added blog on safetensor (#56)

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,5 +13,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms"), require("tailwindcss-animated")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tailwindcss-animated"),
+    require("@tailwindcss/typography"),
+  ],
 };
