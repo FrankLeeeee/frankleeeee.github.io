13fd470b9893857449513eeabb4f93237b9d3cc3
Frank Lee <somerlee.9@gmail.com>
2024-11-10 10:35:03 +0800

    :lipstick: updated blog card style

--- components/Blog/BlogItem.js
+++ components/Blog/BlogItem.js
@@ -4,37 +4,37 @@ import NextLink from "next/link";
 const BlogItem = ({ blog }) => {
   return (
     <div>
-      <div className="col-span-1 shadow-sm mx-4">
-        {/* top side image */}
-        <div className="flex flex-1">
+      <div className="col-span-1 shadow-sm">
+        <div className="flex">
+          {/* top side image */}
           <img
             src={blog.ogImage.url}
             alt="blog"
             lazy="loading"
-            className="object-cover h-40 w-full object-cover"
+            className="object-cover h-24 w-32 object-cover rounded-l-md"
           />
-        </div>
 
-        {/* bottom side content */}
-        <div className="flex flex-1 items-center justify-between truncate rounded-b-md border-b border-r border-t border-gray-200 bg-white h-20">
-          <div className="flex-1 truncate px-4 py-2 text-sm">
-            <NextLink
-              as={`/blogs/${blog.slug}`}
-              href="/blogs/[slug]"
-              className="text-large font-medium text-black hover:text-gray-600"
-            >
-              {blog.title}
-            </NextLink>
+          {/* bottom side content */}
+          <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-l border-gray-300 bg-white h-24">
+            <div className="flex-1 truncate px-4 py-2 text-sm">
+              <NextLink
+                as={`/blogs/${blog.slug}`}
+                href="/blogs/[slug]"
+                className="text-xl font-medium text-black hover:text-gray-600"
+              >
+                {blog.title}
+              </NextLink>
 
-            <p className="text-gray-500 line-clamp-1 truncate">
-              {blog.description}
-            </p>
+              <p className="text-gray-500 line-clamp-1 truncate">
+                {blog.description}
+              </p>
 
-            <div className="flex space-x-2 text-gray-500">
-              <p>{blog.timeReading.text}</p>
-              <p>•</p>
+              <div className="flex space-x-2 text-gray-500">
+                <p>{blog.timeReading.text}</p>
+                <p>•</p>
 
-              <p>{blog.date}</p>
+                <p>{blog.date}</p>
+              </div>
             </div>
           </div>
         </div>
--- components/Blog/BlogList.js
+++ components/Blog/BlogList.js
@@ -2,7 +2,7 @@ const BlogList = ({ children }) => {
   return (
     <div>
       <div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">{children}</div>
+        <div className="grid grid-cols-1 gap-4">{children}</div>
       </div>
     </div>
   );
