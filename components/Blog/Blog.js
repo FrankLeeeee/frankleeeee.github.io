7b54416526d77e5ced3eef421a10246c66749db4
Frank Lee <somerlee.9@gmail.com>
2024-11-30 20:49:34 +0800

    :lipstick: added social media sharing buttons

--- components/Blog/Blog.js
+++ components/Blog/Blog.js
@@ -5,8 +5,30 @@ import rehypeAutolinkHeadings from "remark-autolink-headings";
 import remarkSlug from "remark-slug";
 import remarkExternalLinks from "remark-external-links";
 import { H2, H3, Link, ListItem, Code } from "./MdElements";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TelegramIcon,
+  LinkedinIcon,
+  FacebookIcon,
+  WhatsappIcon,
+  RedditIcon,
+  XIcon,
+} from "react-share";
 
-const Blog = ({ readingTime, title, description, date, ogImage, content }) => {
+const Blog = ({
+  readingTime,
+  title,
+  description,
+  date,
+  ogImage,
+  content,
+  slug,
+}) => {
   return (
     <article className="prose mx-auto text-slate-400">
       <h1 className="text-4xl text-white font-bold">{title}</h1>
@@ -14,8 +36,55 @@ const Blog = ({ readingTime, title, description, date, ogImage, content }) => {
       <h4 className="text-sm text-slate-400">Reading time: {readingTime}</h4>
       <h4 className="text-sm text-slate-400">Description: {description}</h4>
       <img src={ogImage.url} alt="blog" lazy="loa/ding" />
+
+      {/* social media sharing buttons */}
+      <div className="flex items-center gap-2">
+        <span className="text-sm text-slate-400">Share to:</span>
+        <TwitterShareButton
+          url={`https://franklee.xyz/blogs/${slug}`}
+          className="text-slate-400"
+        >
+          <XIcon
+            size={24}
+            round={true}
+            className="rounded-full border border-white"
+          />
+        </TwitterShareButton>
+        <LinkedinShareButton
+          url={`https://franklee.xyz/blogs/${slug}`}
+          className="text-slate-400"
+        >
+          <LinkedinIcon size={24} round={true} />
+        </LinkedinShareButton>
+        <FacebookShareButton
+          url={`https://franklee.xyz/blogs/${slug}`}
+          className="text-slate-400"
+        >
+          <FacebookIcon size={24} round={true} />
+        </FacebookShareButton>
+        <WhatsappShareButton
+          url={`https://franklee.xyz/blogs/${slug}`}
+          className="text-slate-400"
+        >
+          <WhatsappIcon size={24} round={true} />
+        </WhatsappShareButton>
+        <RedditShareButton
+          url={`https://franklee.xyz/blogs/${slug}`}
+          className="text-slate-400"
+        >
+          <RedditIcon size={24} round={true} />
+        </RedditShareButton>
+        <TelegramShareButton
+          url={`https://franklee.xyz/blogs/${slug}`}
+          className="text-slate-400"
+        >
+          <TelegramIcon size={24} round={true} />
+        </TelegramShareButton>
+      </div>
+
       {/* create a line break before the markdown content */}
       <hr className="rounded text-slate-400" />
+
       <Markdown
         className="text-justify"
         remarkPlugins={[remarkGfm, remarkSlug, remarkExternalLinks]}
